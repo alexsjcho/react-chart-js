09a02390ffcb55854a173ad3efe7527586d6c3b6
Alex Cho <alexsjcho@gmail.com>
2019-03-06 13:25:12 -0800

    updated project

--- src/component/Chart.js
+++ src/component/Chart.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Bar, Line, Pie } from "react-chartjs-2";
+import { Bar, Line, Pie, Doughnut, Bubble } from "react-chartjs-2";
 
 class Chart extends Component {
   constructor(props) {
@@ -50,6 +50,36 @@ class Chart extends Component {
         />
 
         <Pie
+          data={this.state.chartData}
+          options={{
+            title: {
+              display: this.props.displayTitle,
+              text: "Largest Cities In " + this.props.location,
+              fontSize: 25
+            },
+            legend: {
+              display: this.props.displayLegend,
+              position: this.props.legendPosition
+            }
+          }}
+        />
+
+        <Doughnut
+          data={this.state.chartData}
+          options={{
+            title: {
+              display: this.props.displayTitle,
+              text: "Largest Cities In " + this.props.location,
+              fontSize: 25
+            },
+            legend: {
+              display: this.props.displayLegend,
+              position: this.props.legendPosition
+            }
+          }}
+        />
+
+        <Bubble
           data={this.state.chartData}
           options={{
             title: {
